161f6c71cd3905c32d37428c10e32bea0b94cba5
Domenico Simone <dome.simone@gmail.com>
2022-12-22 07:55:16 +0100

    Fixed version in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rMLST-API',
-    version='0.1.4',
+    version='0.1.5',
     py_modules=['src.rmlst_api.*'],
     install_requires=[
         'Click',
